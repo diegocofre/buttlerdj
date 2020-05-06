9cb60c44295ec617f4b1f55a18a52dbfc65c5abd
Diego Cofré <diego.cofre@gmail.com>
2020-05-06 11:20:23 -0300

    static settings

--- djwebapi/settings/base.py
+++ djwebapi/settings/base.py
@@ -14,6 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+SITE_ROOT = os.path.dirname(__file__)
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -112,12 +114,13 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
-SITE_ROOT = os.path.dirname(__file__)
 
 TEMPLATE_DIRS = [os.path.join(SITE_ROOT, "templates"),]
+
+
