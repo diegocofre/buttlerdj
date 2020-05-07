31df3a9c2aeff517bfcd9fd6abbf188f358ad67e
Cultivoloco <enerone@gmail.com>
2020-05-07 14:52:29 -0300

    primer commit fabi

--- djwebapi/settings/prod.py
+++ djwebapi/settings/prod.py
@@ -4,7 +4,27 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1'] #, 'mailerProdDNS'
 
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'buttler',         # Or path to database file if using sqlite3.
+        'USER': 'postgres',                      # Not used with sqlite3.
+        'PASSWORD': 'postgres',                  # Not used with sqlite3.
+        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'mysite.sqlite3',         # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -20,7 +40,7 @@ DATABASES = {
 } 
 
 
-
+'''
 """
  LOGGING = {
     'version': 1,
